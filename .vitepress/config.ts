cfadedf01d4a1f3b57679654d74b033b06e07fb8
wangjia <wangjia5289@163.com>
2024-12-30 15:51:06 +0800

    test

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -209,7 +209,7 @@ export default defineConfig({
     },
     nav: [
       { text: '主页', link: '/' },
-      { text: '笔记', link: '/blog/' },
+      { text: '笔记', link: '/笔记/' },
       { text: '最近更新', link: '/toc' },
     ],
     sidebar,
